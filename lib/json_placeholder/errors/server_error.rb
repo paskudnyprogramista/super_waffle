5a3fa4d538e2ce36fc1a0ea8bdfbc34ab136f50b
paskudnyprogramista <paskudnyprogramista@pm.me>
2020-06-15 23:51:48 +0200

    Use OJ to handle response from JSONPlaceholder ApiClient
    Add general error which can be extended later on.

---
+++ lib/json_placeholder/errors/server_error.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module JsonPlaceholder
+  module Errors
+    class ServerError < StandardError; end
+  end
+end
--- lib/json_placeholder/request.rb
+++ lib/json_placeholder/request.rb
@@ -1,24 +1,36 @@
 # frozen_string_literal: true
 
+require 'json_placeholder/errors/server_error'
+
 module JsonPlaceholder
   class Request
     DOMAIN = Rails.application.credentials.jsonplaceholder.fetch(:api_url)
+    RETRY_MAX = 5
 
     def get(resource)
-      retry_max = 3
       retry_count = 0
       endpoint = URI.join(DOMAIN, '/', resource)
 
-      while retry_count <= retry_max
+      while retry_count <= RETRY_MAX
         response = HTTP.get(endpoint)
         status_code = response.code
 
         break if status_code >= 200 && status_code < 300
 
+        sleep(1000)
+
         retry_count += 1
       end
 
-      response
+      handle_response(response)
+    end
+
+    private
+
+    def handle_response(response)
+      raise JsonPlaceholder::Errors::ServerError, respoonse unless response.code < 400
+
+      Oj.load(response.body, symbol_keys: true)
     end
   end
 end
